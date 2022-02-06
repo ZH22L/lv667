4378768a781940287ef169dc876b85bd80eafec7
ZH22L <rezervzheniagulch22@gmail.com>
2022-02-07 00:12:50 +0200

    Add Zhenia  Version

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -15,7 +15,7 @@ public class Appl {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+	                      	System.out.println("Hello! My name is Zhenia");
 		//
 		int i = 456;
 		System.out.println("main i= " + i);
